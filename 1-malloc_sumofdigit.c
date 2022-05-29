55690b3b1d374f9c5ebf16bcdf4fb03c03c63236
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2022-05-29 21:20:08 +0530

    Add files via upload

---
+++ 1-malloc_sumofdigit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+    int i,j,t;
+    int *ptr;
+    printf("enetr n ");
+    int n;
+    scanf("%d",&n);
+
+ptr=(int *)malloc(n*sizeof(int));
+for ( i = 0; i <n; i++)
+{
+    scanf("%d",ptr+i);
+}
+
+int sum=0, avg=0;
+for ( i = 0; i <n; i++)
+{
+    sum=sum+ (*(ptr+i));
+}
+
+printf("%d is sum \n",sum);
+printf("%d is avg ",sum/n);
+
+
+return 0;
+}
