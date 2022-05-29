b14fb71b5218b32998dfacc444fe8d23f1b9ed51
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2022-05-29 21:19:04 +0530

    Add files via upload

---
+++ 2_sumofdigit_refer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void sod(int *,int );
+
+int main(){
+    int n,i,s=0;
+    printf("enter number ");
+    scanf("%d",&n);
+
+    sod(&n,sum);
+    printf("%d is sum ",sum);
+
+return 0;
+}
+
+void sum(, int)
+int r,j, sum=0;
+{
+   while (j!=0)
+   {
+       r=
+   }
+    
+}
